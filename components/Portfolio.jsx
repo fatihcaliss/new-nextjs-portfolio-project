ae399ef95fb2cc2018022b5c88a19c6cf0672827
fatihcaliss <fthcls@gmail.com>
2022-11-28 01:16:45 +0300

    static prop func added

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,10 +1,46 @@
 import Image from 'next/image';
 import React from 'react';
 
-const Portfolio = () => {
+const Portfolio = ({ repos }) => {
     return (
         <div className='max-w-[1240px] mx-auto py-16 text-center'>
-            <h1 className='font-bold text-2xl p-4'>Travel Photos</h1>
+
+            {/* <div className="px-4 py-8 md:py-32 space-y-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 "> */}
+            <div className="px-4 py-8 md:py-32 space-y-4 bg-[transparent]">
+                <aside className="space-y-2">
+                    <h2 className="text-6xl font-semibold text-white bg-black bg-opacity-80">GitHub Repositories</h2>
+                </aside>
+                <div className="grid  md:grid-cols-3 gap-4  ">
+
+                    {repos.map((repo, index) => {
+                        return (
+                            <div key={index} className="shadow dark:shadow-none p-4 rounded border-white border bg-black bg-opacity-90">
+                                <article className="flex flex-col mb-2">
+                                    {/* <img src="https://picsum.photos/200/300" alt="blog" className="w-full h-48 object-cover object-center rounded-md" /> */}
+                                    <h3 className="m-1">
+                                        <a href="#" className="text-white title-font text-xl font-medium">{repo?.name}</a>
+                                    </h3>
+                                    <p className="mt-1 font-light text-sm text-gray-100">{repo.description ? repo.description : "There is no description."}</p>
+                                </article>
+                                <span className="hover:text-gray-800 dark:text-gray-400 text-gray-500 group">
+                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                        <a href={repo.html_url} target={"_blank"} className="inline-flex items-center">Go to code
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className=" w-4 h-4 group-hover:translate-x-1">
+                                                <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd"></path>
+                                            </svg>
+                                        </a>
+                                    </button>
+
+                                </span>
+                            </div>
+                        )
+                    })}
+
+                </div>
+            </div>
+
+
+            {/* <h1 className='font-bold text-2xl p-4'>Travel Photos</h1>
             <div className='grid grid-rows-none md:grid-cols-5 p-4 gap-4'>
                 <div className='w-full h-full col-span-2 md:col-span-3 row-span-2'>
                     <Image
@@ -55,7 +91,7 @@ const Portfolio = () => {
                         objectFit='cover'
                     />
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Portfolio from '../components/Portfolio'
 
-const projects = () => {
+const projects = ({ repos }) => {
     return (
         <div>
-            <Hero heading='Projects' message='This is some of my recent work traveling the world.' />
-            <Portfolio />
+            {/* <Hero heading='Projects' message='This is some of my recent work traveling the world.' /> */}
+            <div className='flex items-center justify-center bg-fixed bg-center bg-cover custom-bg-project'>
+                {/* Overlay */}
+                {/* <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' /> */}
+
+                <Portfolio repos={repos} />
+            </div>
+
         </div>
     )
 }
 
 export default projects
+
+export const getStaticProps = async () => {
+    const res = await fetch("https://api.github.com/users/fatihcaliss/repos");
+    const data = await res.json();
+    return {
+        props: {
+            repos: data
+        }
+    }
+
+
+}
