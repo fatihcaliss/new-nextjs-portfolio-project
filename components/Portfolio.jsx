311c1a10dc16e113e01bc11bf4420643afbcd930
fatihcaliss <fthcls@gmail.com>
2022-11-28 10:37:01 +0300

    taget blank changed

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -24,7 +24,7 @@ const Portfolio = ({ repos }) => {
                                 </article>
                                 <span className="hover:text-gray-800 dark:text-gray-400 text-gray-500 group">
                                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                        <a href={repo.html_url} target={"_blank"} className="inline-flex items-center">Go to code
+                                        <a href={repo.html_url} className="inline-flex items-center">Go to code
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className=" w-4 h-4 group-hover:translate-x-1">
                                                 <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd"></path>
                                             </svg>
